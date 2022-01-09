c3f78302990f6a87ca0a20e6192b13b62227c530
mehmetdemircioglu <demircioglumhmt@gmail.com>
2022-01-09 14:51:17 +0300

    step1 add View in app.js

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 40 }}>
+      <View>
+        <TextInput
+          placeholder='Enter Word'
+          style={{ borderColor: 'black', borderWidth: 1, padding: 10 }} />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
