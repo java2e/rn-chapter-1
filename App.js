9e97e4a1da126d5fce7edeafd707984134414875
mehmetdemircioglu <demircioglumhmt@gmail.com>
2022-01-09 15:15:02 +0300

    style edit

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={{ padding: 40 }}>
-      <View>
+      <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
         <TextInput
           placeholder='Enter Word'
-          style={{ borderColor: 'black', borderWidth: 1, padding: 10 }} />
+          style={{ width:'80%', borderColor: 'black', borderWidth: 1, padding: 10 }} />
         <Button title="ADD" />
       </View>
       <View>
@@ -17,11 +17,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
